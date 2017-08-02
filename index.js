be4d7867ee6a4923100fb70b26dd4f2ba11fee74
lexiht <hazukitran@gmail.com>
2017-08-02 20:12:20 +0100

    Find the title with the highest % of likes

--- index.js
+++ index.js
@@ -5,4 +5,25 @@ fetch(url)
 .then((response) => response.json())
 .then((data) => {
   let videos = data.videos;
+  titleOfHighestPercentageLikes(videos);
 })
+
+const percentOfLikes = (likes, dislikes) => {
+  return likes / (likes + dislikes) * 100;
+};
+
+const titleOfHighestPercentageLikes = (videos) => {
+  let highest = {
+    percent: 0,
+    title: undefined
+  };
+  videos.forEach((video) => {
+    let percent = percentOfLikes(video.likes, video.dislikes);
+    if (percent > highest.percent) {
+      highest.percent = percent;
+      highest.title = video.title;
+    }
+  })
+  console.log('Title of highest % of likes =>', highest.title);
+  return highest.title;
+};
