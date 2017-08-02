ba30804c91003a7e0f26101fc6172c499be1d2a1
lexiht <hazukitran@gmail.com>
2017-08-02 20:24:25 +0100

    Find mean % of likes, total views and average time between each video posted

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ fetch(url)
 .then((data) => {
   let videos = data.videos;
   titleOfHighestPercentageLikes(videos);
+  meanPercentOfLikes(videos);
+  totalOfViews(videos);
+  averageTimeInterval(videos);
 })
 
 const percentOfLikes = (likes, dislikes) => {
@@ -27,3 +30,42 @@ const titleOfHighestPercentageLikes = (videos) => {
   console.log('Title of highest % of likes =>', highest.title);
   return highest.title;
 };
+
+const meanPercentOfLikes = (videos) => {
+  let total = 0;
+  videos.forEach((video) => {
+    total += percentOfLikes(video.likes, video.dislikes);
+  })
+  let mean = Math.round(total / videos.length);
+  console.log('The mean % of all videos =>', mean + '%');
+  return mean;
+}
+
+const totalOfViews = (videos) => {
+  let views = 0;
+  videos.forEach((video) => {
+    views += video.views;
+  })
+  console.log('Total of views of all videos =>', views.toLocaleString() + ' views');
+  return views;
+}
+
+const totalTimeDifference = (times) => {
+  let diff = times.slice(1).map((number, index) => {
+    return Math.abs(number - times[index]);
+  });
+  let totalDiff = diff.reduce((sum, value) => {
+    return sum + value;
+  });
+  return totalDiff;
+}
+
+const averageTimeInterval = (videos) => {
+  let times = videos.map((video) => {
+    return new Date(video.published_at).getTime();
+  });
+  let average = totalTimeDifference(times) / (times.length - 1);
+  let diffDays = Math.ceil(average / (1000 * 3600 * 24));
+  console.log('The average time between each video posted =>', diffDays + ' days');
+  return diffDays;
+}
